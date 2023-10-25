773418fd864187e934b78e3e061ba284e4e73857
b1n0xD <lucianovieiradev@gmail.com>
2023-10-24 23:11:35 -0300

    feature: insert random module and create execSearch function with urls

--- google.py
+++ google.py
@@ -1,6 +1,13 @@
-from search import *;
+from search import *
+import random
 
 #construtor
 class Google (Browser):
     def __init__(self):
         super().__init__();
+        def execSearch(self, words):
+            print("Esta pesquisando..." , words);
+            pages = ["https://google.com/","https://google.com.br/","https://google.fr/","https://google.jp/"];
+            page = pages[random.randint(0, len(pages) -1)];
+            print("A pagina sera: " + page);
+            
