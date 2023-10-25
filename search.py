a1b0ab1b079308dbdf2304901e0dafdfe81fa29b
b1n0xD <lucianovieiradev@gmail.com>
2023-10-24 22:53:54 -0300

    feature: include function to close automation process.

--- search.py
+++ search.py
@@ -1,14 +1,14 @@
 import random
-
+import selenium
 import webdriver_manager
 
 class Browser:
     def __init__(self):
         chrome_options = webdriver_manager.ChromeOptions();
         user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-                       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-                       "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-                       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+                       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+                       "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+                       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
                        "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.111 Mobile Safari/537.36"                       
                        ]; 
         agent = user_agents[random.randint(0, len(user_agents) - 1)];
@@ -16,4 +16,10 @@ class Browser:
         chrome_options.add_argument("--headless");
         chrome_options.add_argument("--no-sandbox");
         chrome_options.add_argument("--mute-audio");
-        self.driver = webdriver.Chrome();
+        self.driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options);
+        
+        def execSearch(self, words):
+            print("Metodo nao implementado...");
+            
+        def __del__(self):
+            self.driver.close();
