1e4a83aa8ae038a1a6e19925b8a67730da570900
b1n0xD <lucianovieiradev@gmail.com>
2023-10-24 22:42:05 -0300

    feature: include random user agents.

--- search.py
+++ search.py
@@ -1,1 +1,15 @@
-print("teste");
+import random
+
+import webdriver_manager
+
+class Browser:
+    def __init__(self):
+        chrome_options = webdriver_manager.ChromeOptions();
+        user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+                       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+                       "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+                       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+                       "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.111 Mobile Safari/537.36"                       
+                       ]; 
+        agent = user_agents[random.randint(0, len(user_agents) - 1)];
+        chrome_options.add_argument("user-agent=" + user_agents);
